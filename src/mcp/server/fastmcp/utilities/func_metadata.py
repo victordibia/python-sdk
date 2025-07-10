0a4e8d4e17a51772f564c6e804e589061d4f7636
Giuseppe Coviello <gcov@protonmail.com>
2025-07-10 17:36:45 -0400

    Avoiding to parse string arguments containing valid JSON if the argument annotation is str (#1113)

--- src/mcp/server/fastmcp/utilities/func_metadata.py
+++ src/mcp/server/fastmcp/utilities/func_metadata.py
@@ -130,7 +130,7 @@ class FuncMetadata(BaseModel):
         for field_name in self.arg_model.model_fields.keys():
             if field_name not in data.keys():
                 continue
-            if isinstance(data[field_name], str):
+            if isinstance(data[field_name], str) and self.arg_model.model_fields[field_name].annotation is not str:
                 try:
                     pre_parsed = json.loads(data[field_name])
                 except json.JSONDecodeError:
